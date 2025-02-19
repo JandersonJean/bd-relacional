425b99797eb4ff6d4e7116fd2a861b21265cf016
Janderson Jean <jandersonjean@gmail.com>
2025-02-19 05:37:37 -0300

    Inserindo dados - Teste com auto increment

---
+++ insertNovoEstado.sql
@@ -0,0 +1,5 @@
+insert into estados (id, nome, sigla, regiao, populacao)
+values (1000, 'Novo','NV','Norte','2.53');
+
+insert into estados (nome, sigla, regiao, populacao)
+values ('Mais Novo','MN','Sul','2.63');
